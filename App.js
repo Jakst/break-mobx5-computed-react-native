7de19aded6e95261b57b3686e9fba2d0672b32bc
Jakob Ståhl <jakob.stahl91@gmail.com>
2018-12-03 10:46:25 +0100

    Working observable setup

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
+import { observable } from "mobx";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
+  @observable a = 1;
+  @observable b = 2;
+
   render() {
     return (
       <View style={styles.container}>
+        <Text>
+          {this.a} + {this.b}
+        </Text>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
